d8b4dc3b5d5f0e99c8aa26ea3ab594994dd41140
Tolusophy <tioluwaniaremu@gmail.com>
2025-05-05 10:06:16 +0400

    blog update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -121,7 +121,7 @@ export default function Home() {
               specializing in advancing artificial intelligence with a robust
               skill set spanning both research and industry. */}
               It seems like I&apos;ve gotten your attention! Hey there! I&apos;m
-              Toluwani, your friendly neighborhood AI enthusiast. I build and
+              Toluwani, your friendly neighborhood sp-AI-derman. I build and
               write AI stuff sometimes. I am also a Machine Learning PhD Candidate
               at <span> </span>
               <Link href="https://mbzuai.ac.ae/" className=" text-primary">
